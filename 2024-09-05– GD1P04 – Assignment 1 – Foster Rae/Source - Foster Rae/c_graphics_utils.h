e0b3547d9129eab33885ede6e89e39b1c6196183
FosterRae <fosterjameskiatarae@gmail.com>
2024-10-06 21:35:47 +1300

    Removed all instance specific code.
    Pipeline is now empty of all objects and fresh to start a new exercise.

--- 2024-09-05– GD1P04 – Assignment 1 – Foster Rae/Source - Foster Rae/c_graphics_utils.h
+++ 2024-09-05– GD1P04 – Assignment 1 – Foster Rae/Source - Foster Rae/c_graphics_utils.h
@@ -39,28 +39,4 @@ public:
 	 * @return A pointer to the created window.
 	 */
 	static GLFWwindow* create_window(int width, int height, const char* title);
-	/**
-	 * @brief Initializes the quads.
-	 * @note Creates two quads at the bottom left and bottom right of the screen,
-	 *	     with textures applied to them.
-	 *		 \n Create the needed objects globally and pass them in.
-	 *
-	 * @param quad_vao The vertex array object for the quad.
-	 * @param quad_vbo The vertex buffer object for the quad.
-	 * @param quad_ebo The element buffer object for the quad.
-	 * @param texture_id The texture ID for the quad.
-	 * @return A GLuint to the created shader program.
-	 */
-	GLuint static initialize_quads(GLuint &quad_vao, GLuint &quad_vbo, GLuint &quad_ebo, GLuint &texture_id);
-	/**
-	 * @brief Initializes the hexagons.
-	 * @note Creates two spinning multi-coloured hexagons.
-	 *		 \n Create the needed objects globally and pass them in.
-	 *
-	 * @param hex_vao The vertex array object for the hexagon.
-	 * @param hex_vbo The vertex buffer object for the hexagon.
-	 * @param hex_ebo The element buffer object for the hexagon.
-	 * @return A GLuint to the created shader program.
-	 */
-	GLuint static initialize_hexagons(GLuint &hex_vao , GLuint &hex_vbo, GLuint &hex_ebo);
 };
--- 2024-09-05– GD1P04 – Assignment 1 – Foster Rae/Source - Foster Rae/main.cpp
+++ 2024-09-05– GD1P04 – Assignment 1 – Foster Rae/Source - Foster Rae/main.cpp
@@ -19,25 +19,8 @@ int window_height = 800;
 GLFWwindow* window;
 GLfloat current_time;
 
-// == Objects ==
-// Quad objects.
-GLuint quad_vao, quad_vbo, quad_ebo, alien_texture_id, program_quad;
-// Hexagon objects.
-GLuint hex_vao, hex_vbo, hex_ebo, program_hex;
-// Hexagon colors.
-glm::vec3 colors[] = {
-    glm::vec3(1.0f, 0.0f, 0.0f), // Red
-    glm::vec3(0.0f, 1.0f, 0.0f), // Green
-    glm::vec3(0.0f, 0.0f, 1.0f), // Blue
-    glm::vec3(1.0f, 1.0f, 0.0f), // Yellow
-    glm::vec3(1.0f, 0.0f, 1.0f), // Magenta
-    glm::vec3(0.0f, 0.0f, 0.0f)  // Black
-};
-// Transformations.
-c_transformations quad1_transform(glm::vec3(-0.35f, -0.5f, 0.0f), 0.0f, glm::vec3(0.5f, 0.5f, 1.0f));
-c_transformations quad2_transform(glm::vec3(0.4f, -0.5f, 0.0f), 0.0f, glm::vec3(0.5f, 0.5f, 1.0f));
-c_transformations hexagon1_transform(glm::vec3(-0.35f, 0.5f, 0.0f), 0.0f, glm::vec3(0.5f, 0.5f, 1.0f));
-c_transformations hexagon2_transform(glm::vec3(0.45f, 0.5f, 0.0f), 0.0f, glm::vec3(0.3f, 0.3f, 1.0f));
+// == TODO: Objects ==
+// TODO: Transformations.
 
 // == Function Prototypes ==
 void initial_setup();
@@ -73,10 +56,7 @@ int main()
 
 void initial_setup()
 {
-    // Create the quads.
-    program_quad = c_graphics_utils::initialize_quads(quad_vao, quad_vbo, quad_ebo, alien_texture_id);
-    // Create the hexagons.
-    program_hex = c_graphics_utils::initialize_hexagons(hex_vao, hex_vbo, hex_ebo);
+	// TODO: Set up objects.
 
 	// Prepare the window.
 	glClearColor(0.56f, 0.57f, 0.60f, 1.0f); // Set the clear color to a light grey.
@@ -84,11 +64,7 @@ void initial_setup()
 }
 void update()
 {
-	// Update objects.
-	quad1_transform.update();
-	quad2_transform.update();
-	hexagon1_transform.update();
-	hexagon2_transform.update();
+	//TODO: Update objects.
 
 	// Poll for and process events.
 	glfwPollEvents();
@@ -102,70 +78,6 @@ void render()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// == START OF RENDERING PIPELINE ==
-
-
-	// == QUADS ==
-	// Quad texture.
-	glActiveTexture(GL_TEXTURE0); // Activate texture unit 0.
-	glBindTexture(GL_TEXTURE_2D, alien_texture_id); // Bind the texture object to the target slot.
-	glUniform1i(glGetUniformLocation(program_quad, "texture_0"), 0); // Set the texture sampler to use texture unit 0.
-	// Use the quad program.
-	glUseProgram(program_quad); // Start using the program object. This activates the shader program.
-	glBindVertexArray(quad_vao); // Bind to the VAO target slot. Subsequent VAO operations will affect this VAO.
-	// Quad uniforms.
-	GLint model_matrix_loc = glGetUniformLocation(program_quad, "model_matrix"); // Get the location of the model matrix uniform.
-
-	// == QUAD 1 ==
-	// Change address mode to mirrored repeat.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT); // Call again invert
-	// Pass the model matrix to the shader AND draw the quad.
-	glUniformMatrix4fv(model_matrix_loc, 1, GL_FALSE, glm::value_ptr(quad1_transform.get_model_matrix()));
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
-	// == QUAD 2 ==
-	// reset the address mode.
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// Pass the model matrix to the shader AND draw the quad.
-	glUniformMatrix4fv(model_matrix_loc, 1, GL_FALSE, glm::value_ptr(quad2_transform.get_model_matrix()));
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
-
-	// == HEXAGONS ==
-	// Use the hexagon program.
-	glUseProgram(program_hex); // Start using the program object. This activates the shader program.
-	glBindVertexArray(hex_vao); // Bind to the VAO target slot. Subsequent VAO operations will affect this VAO.
-	// Hexagon uniforms.
-	model_matrix_loc = glGetUniformLocation(program_hex, "model_matrix"); // Get the location of the model matrix uniform.
-
-	// == HEXAGON 1 ==
-	// Pass the model matrix to the shader AND draw the hexagon.
-	glUniformMatrix4fv(model_matrix_loc, 1, GL_FALSE, glm::value_ptr(hexagon1_transform.get_model_matrix()));
-	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, nullptr);
-
-	// == HEXAGON 2 ==
-	// Pass the model matrix to the shader AND draw the hexagon.
-	glUniformMatrix4fv(model_matrix_loc, 1, GL_FALSE, glm::value_ptr(hexagon2_transform.get_model_matrix()));
-	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, nullptr);
-
-	// == HEXAGON COLORS ==
-	// Send the array of colors to the shader.
-    GLint colors_loc = glGetUniformLocation(program_hex, "colors");
-    glUniform3fv(colors_loc, 7, glm::value_ptr(colors[0]));
-	// Send the input color to the shader.
-    GLint input_color_loc = glGetUniformLocation(program_hex, "input_color");
-    glUniform3f(input_color_loc, 1.0f, 0.0f, 0.0f); // Example: Red color
-    // Send the blend color to the shader.
-    GLint blend_color_loc = glGetUniformLocation(program_hex, "blend_color");
-	// Loop through the colors and send them to the shader
-	for (const auto& color : colors) {
-	    glUniform3f(blend_color_loc, color.r, color.g, color.b);
-	}
-	// Send the current time to the shader.
-	GLint current_time_loc = glGetUniformLocation(program_hex, "current_time");
-	glUniform1f(current_time_loc, current_time);
-
 
 	// == END OF RENDERING PIPELINE ==
 	glBindVertexArray(0); // Unbind the VAO.
